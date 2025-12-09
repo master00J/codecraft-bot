7ccf9dbae9002b422dd7393ed508f808297329d4
master00J <jasonploegaerts@gmail.com>
2025-12-09 18:47:18 +0100

    Change Submit Profile button label to Submit and explain 25 options limit solution

--- modules/comcraft/user-profiles/manager.js
+++ modules/comcraft/user-profiles/manager.js
@@ -144,7 +144,7 @@ class UserProfileManager {
         .addComponents(
           new ButtonBuilder()
             .setCustomId(`profile_submit:${form.id}`)
-            .setLabel('Submit Profile')
+            .setLabel('Submit')
             .setStyle(ButtonStyle.Success)
         );
       components.push(submitRow);
